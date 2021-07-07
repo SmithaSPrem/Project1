7573784b3e62228c3b62c45a9207abfc946f9064
User <user@ignitarium.com>
2021-07-07 04:11:49 -0400

    Edited

--- div.py
+++ div.py
@@ -1,6 +1,6 @@
-S1=250
-S2=25
+num1=250
+num2=25
 
-div=S1/S2;
+div=num1/num2;
 
-print("The division of {0} and {1} is: {2}".format(S1,S2,div))
+print("The division of {0} and {1} is: {2}".format(num1,num2,div))
